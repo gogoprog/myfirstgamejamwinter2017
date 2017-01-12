e96812b2d50db3b9b9760395426404fc0ea31f29
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-12 20:55:40 +0100

    Wip

--- src/systems/PlayerInputSystem.hx
+++ src/systems/PlayerInputSystem.hx
@@ -1,6 +1,7 @@
 package systems;
 
 import ash.tools.ListIteratingSystem;
+import ash.core.NodeList;
 
 import components.*;
 import nodes.*;
@@ -12,6 +13,7 @@ class PlayerInputSystem extends ListIteratingSystem<PlayerInputNode>
 {
     private var engine:Engine;
     private var cameraEntity:Entity;
+    private var charactersList:NodeList<CharacterNode>;
 
     public function new(cameraEntity)
     {
@@ -24,6 +26,8 @@ class PlayerInputSystem extends ListIteratingSystem<PlayerInputNode>
         engine = engine_;
 
         super.addToEngine(engine);
+
+        charactersList = engine.getNodeList(CharacterNode);
     }
 
     private function updateNode(node:PlayerInputNode, dt:Float):Void
@@ -34,20 +38,68 @@ class PlayerInputSystem extends ListIteratingSystem<PlayerInputNode>
         var mouseScreenPosition = new Vector2(mousePosition.x / 640, mousePosition.y / 480);
         var mouseWorldPosition:Vector3 = cameraEntity.get(Camera).screenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, 0));
 
-        if(input.getMouseButtonDown(1 << 2))
+        var closestNode:CharacterNode = null;
+        var closestDistance:Float = Math.POSITIVE_INFINITY;
+
+        for(c in charactersList)
         {
-            node.character.moveTarget = new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
+            c.sprite.setAlpha(1);
         }
 
-        if(input.getMouseButtonPress(1 << 0))
+        for(c in charactersList)
         {
-            if(Math.random() < 0.5)
+            if(c.entity != node.entity && c.character.life > 0)
             {
-                node.character.nextHitAnimation = Factory.animations["punch"];
+                var p2 = c.entity.position;
+                if(Math.abs(p2.x - mouseWorldPosition.x) < 40 && mouseWorldPosition.y > p2.y - 48 && mouseWorldPosition.y < p2.y + 32)
+                {
+                    var distance = Maths.getVector3DistanceSquared(mouseWorldPosition, p2);
+
+                    if(distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestNode = c;
+                    }
+                }
+            }
+        }
+
+        if(closestNode != null)
+        {
+            var playerPos = node.entity.position;
+            var closestPos = closestNode.entity.position;
+            var distance = Maths.getVector3DistanceSquared(closestPos, playerPos);
+            closestNode.sprite.setAlpha(0.4);
+
+            if(distance < 40 * 40)
+            {
+                if(input.getMouseButtonPress(1 << 0))
+                {
+                    if(Math.random() < 0.5)
+                    {
+                        node.character.nextHitAnimation = Factory.animations["punch"];
+                    }
+                    else
+                    {
+                        node.character.nextHitAnimation = Factory.animations["punches"];
+                    }
+                }
             }
             else
             {
-                node.character.nextHitAnimation = Factory.animations["punches"];
+                var xSign = (playerPos.x - closestPos.x) / Math.abs(playerPos.x - closestPos.x);
+
+                if(input.getMouseButtonPress(1 << 0))
+                {
+                    node.character.moveTarget = new Vector2(closestPos.x + 32 * xSign, closestNode.element.y);
+                }
+            }
+        }
+        else
+        {
+            if(input.getMouseButtonDown(1 << 0))
+            {
+                node.character.moveTarget = new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
             }
         }
 
