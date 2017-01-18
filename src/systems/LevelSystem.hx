0c91f095eaa79a190c8a7b541f16858b27ea9cb8
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-18 14:56:23 +0100

    Wip

--- src/systems/LevelSystem.hx
+++ src/systems/LevelSystem.hx
@@ -12,8 +12,12 @@ class LevelSystem extends ListIteratingSystem<LevelNode>
 {
     private var engine:Engine;
     private var cameraEntity:Entity;
+    private var loop:Int;
     private var zone:Int;
     private var levelCount:Int = 2;
+    private var totalLength:Int;
+    private var levelLength:Int;
+    private var factor:Float;
 
     public function new(cameraEntity)
     {
@@ -39,19 +43,36 @@ class LevelSystem extends ListIteratingSystem<LevelNode>
         e.scale = new Vector3(1, 1, 1);
         e.position = new Vector3(-400, -400, 0);
         e.get(TileMap2D).setTmxFile(Gengine.getResourceCache().getTmxFile2D("map.tmx", true));
+
+        levelLength = 48*64;
+        totalLength = levelLength * levelCount;
     }
 
     public override function update(dt:Float):Void
     {
         var camPos = cameraEntity.position;
-        zone = Std.int(camPos.x / (48*64 * levelCount));
+        loop = Std.int(camPos.x / totalLength);
+        zone = Std.int(camPos.x / levelLength);
+        zone = zone % levelCount;
+
+        factor = (camPos.x % levelLength) / levelLength;
 
         super.update(dt);
     }
 
     private function updateNode(node:LevelNode, dt:Float):Void
     {
-        node.entity.position = new Vector3(-400 + 48*64 * node.level.index + 48*64*levelCount*zone, -400, 0);
+        var l = node.level.index;
+
+        node.entity.position = new Vector3(-400 + levelLength*l + totalLength*loop, -400, 0);
+
+        if(factor < 0.5)
+        {
+            if(l == levelCount - 1 && zone == 0)
+            {
+                node.entity.position = new Vector3(-400 + levelLength*l + totalLength*(loop - 1), -400, 0);
+            }
+        }
     }
 
     private function onNodeAdded(node:LevelNode)
