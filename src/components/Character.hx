a8971618980c5fd7ea3ac56bff2d7800421f29e8
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-19 13:47:54 +0100

    Updated map

--- src/components/Character.hx
+++ src/components/Character.hx
@@ -7,7 +7,7 @@ import ash.fsm.*;
 class Character
 {
     public var sm:EntityStateMachine;
-    public var moveSpeed:Float = 2500.0;
+    public var moveSpeed:Float = 250.0;
     public var moveTarget:Vector2 = null;
     public var life:Float = 100.0;
     public var mustAttack:Bool;
